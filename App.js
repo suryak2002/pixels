7dc0219adca6c3f93307f84099afe4e1dfcbefaa
suryak2002 <116099435+suryak2002@users.noreply.github.com>
2022-11-26 20:15:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+// ** React Import
+import React from "react";
+import "./App.css";
+
+// ** App js component Import\
+import MovieLists from "./movieComponent/MovieLists";
+
+function App() {
+  return (
+    <div className="App">
+      <MovieLists />
+    </div>
+  );
+}
+
+export default App;
